e6b738d6d72e09cfc029c0b4209efc4b21be909a
Fbaygul <fbaygulf@gmail.com>
2023-11-22 21:59:47 +0300

    add what is ori and some training videos

--- lib/pages/content_pages/trainings.dart
+++ lib/pages/content_pages/trainings.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class Trainings extends StatelessWidget {
+class Trainings extends StatefulWidget {
+  @override
+  _TrainingsState createState() => _TrainingsState();
+}
+
+class _TrainingsState extends State<Trainings> {
+  // List of video IDs
+  final List<String> _videoIds = [
+    'zmNNxdLBaf8', // Example YouTube video ID
+    'P6EtYMHL7Wc', // Example YouTube video ID
+  ];
+
+  late List<YoutubePlayerController> _controllers;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllers = _videoIds.map<YoutubePlayerController>((videoId) {
+      return YoutubePlayerController(
+        initialVideoId: videoId,
+        flags: YoutubePlayerFlags(
+          autoPlay: false,
+          hideThumbnail: false,
+        ),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Trainings.'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Training Videos'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: _controllers.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: YoutubePlayerBuilder(
+                player: YoutubePlayer(
+                  controller: _controllers[index],
+                  showVideoProgressIndicator: true,
+                ),
+                builder: (context, player) {
+                  return Column(
+                    children: [
+                      // some widgets
+                      player,
+                      //some other widgets
+                    ],
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    // Dispose all controllers to avoid memory leaks
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
   }
 }
--- lib/pages/content_pages/what_is_orienteering.dart
+++ lib/pages/content_pages/what_is_orienteering.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class WhatIsOrienteering extends StatelessWidget {
+  Future<String> loadAsset(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Here is information about what orienteering is.'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            FutureBuilder(
+              future: loadAsset('assets/orienteering/ori1.txt'),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Text(snapshot.data ?? 'Metin yüklenemedi.');
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            SizedBox(height: 16.0),
+            Image.asset(
+              'assets/orienteering/orienteering.png',
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 16.0),
+            FutureBuilder(
+              future: loadAsset('assets/orienteering/ori2.txt'),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Text(snapshot.data ?? 'İkinci metin yüklenemedi.');
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
