ab5754136ae59517a8023f9e7c692b1de4e3ae16
Fbaygul <fbaygulf@gmail.com>
2023-11-27 23:38:19 +0300

    add faq

--- lib/pages/content_pages/compass_use.dart
+++ lib/pages/content_pages/compass_use.dart
@@ -16,22 +16,22 @@ class CompassUse extends StatelessWidget {
             Divider(height: 20, thickness: 2),
             Text(
               'Pusulada Açı Ayarlamak: Bu işlem, pusulanın hedefe doğru yönlendirilmesini sağlayarak, kullanıcının belirli bir yönü takip etmesine olanak tanır. Pusuladaki döner halka, belirlenen açıya ayarlanır.',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(),
             ),
             SizedBox(height: 16),
             Text(
               'Harita Üzerinde Pusula Açısı Ayarlamak: Harita üzerindeki yönlerin pusuladaki açılarla eşleştirilmesi işlemidir. Haritanın manyetik kuzeyi pusulanın kuzeyi ile hizalanacak şekilde yerleştirilir ve ardından rotanız üzerindeki açı, pusula üzerinde ayarlanır.',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(),
             ),
             SizedBox(height: 16),
             Text(
               'Kerteriz Almak – Arazide Açı Ölçmek: Kerteriz almak, pusulanın görüş hattını kullanarak, belirli bir nesne veya noktaya doğru yönlendirilmesi işlemidir. Arazide bir nesneye doğru olan açıyı ölçmek için kullanılır.',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(),
             ),
             SizedBox(height: 16),
             Text(
               'Kerteriz Takip Etmek: Bir kerteriz hattı belirledikten sonra, bu hatta sadık kalarak hedefe doğru ilerlemeyi ifade eder. Pusulayı sabit tutarak ve hedef alınan nesneyi sürekli göz önünde bulundurarak doğru yolda kalınır.',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(),
             ),
           ],
         ),
--- lib/pages/content_pages/faq.dart
+++ lib/pages/content_pages/faq.dart
@@ -3,8 +3,59 @@ import 'package:flutter/material.dart';
 class FAQ extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('FAQ.'),
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          ExpansionTile(
+            title: Text('Oryantiring nedir?'),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      'Oryantiring, harita ve pusula kullanarak belirlenmiş kontrol noktalarını en kısa sürede bulmayı hedefleyen bir doğa sporudur. Hem fiziksel hem de zihinsel becerileri geliştirir.')),
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Oryantiring için ne tür ekipmanlara ihtiyaç vardır?'),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      'Oryantiring için temel ekipmanlar arasında özel olarak hazırlanmış oryantiring haritası, pusula, uygun spor kıyafetleri ve ayakkabılar yer alır. Ayrıca, yarışmaya bağlı olarak zaman ölçüm cihazı (SI kartı) gerekebilir.')),
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Oryantiringe nasıl başlayabilirim?'),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      'Oryantiringe başlamak için, yerel oryantiring kulüplerine katılabilir veya oryantiring etkinliklerine katılarak başlayabilirsiniz. İlk adım olarak, basit parkurlarla ve eğitim etkinlikleriyle deneyim kazanmak yararlı olacaktır.')),
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Oryantiring hangi yaş grupları için uygundur?'),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      'Oryantiring, tüm yaş grupları için uygundur. Çocuklar için eğitici parkurlar, yetişkinler için rekabetçi yarışlar ve yaşlılar için daha az zorlayıcı rotalar bulunmaktadır')),
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Oryantiring yarışmaları ne kadar sürer?'),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      'Oryantiring yarışmalarının süresi düzenlenen parkurun uzunluğuna ve zorluk derecesine bağlı olarak değişir. Kısa parkurlar 20-30 dakika sürebilirken, uzun mesafe yarışmaları 1 saatten fazla sürebilir.')),
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Oryantiring sadece ormanda mı yapılır?'),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      'Hayır, oryantiring ormanlık alanların yanı sıra parklar, kentsel alanlar ve dağlık bölgeler gibi farklı ortamlarda da yapılabilir. Harita ve parkur düzenlemesi yapılan alana göre farklılık gösterir.')),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
